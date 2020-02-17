b1ecbac7f14fad8bac987a0f09c71e62ffe66d2b
mariabaltabaeva <maria.baltabaeva@gmail.com>
2020-02-17 11:58:56 -0500

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root "signups#new"
+
+  get "/thanks" => "pages#thanks"
+  
+  resources :signups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
